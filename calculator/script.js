6c9f36a668b5d58ecee90137a03f655174d84c76
Renan Pimenta <renan_01234@hotmail.com>
2023-01-09 22:22:29 -0300

    fix sequential bug

--- calculator/script.js
+++ calculator/script.js
@@ -2,291 +2,138 @@ const number7 = document.getElementById('number-seven')
 const number8 = document.getElementById('number-eight')
 const number9 = document.getElementById('number-nine')
 const number6 = document.getElementById('number-six')
-const number5 = document.getElementById('number-five')          // <--- bloco onde seleciona o botao
+const number5 = document.getElementById('number-five')          // <--- number selection constants
 const number4 = document.getElementById('number-four')
 const number3 = document.getElementById('number-three')
 const number2 = document.getElementById('number-two')
 const number1 = document.getElementById('number-one')
 const number0 = document.getElementById('number-zero')
-const numeros = document.querySelectorAll('.number')
 
 const point = document.getElementById('point')
 
 const percented = document.getElementById ('%')
 const cleared = document.getElementById('clear')
 const cleared2 = document.getElementById('ce')
-const plus = document.getElementById('plus')
+const plus = document.getElementById('plus')                     // <---   operator selection constants
 const equals = document.getElementById('equals')
 const divided = document.getElementById('divided')
 const multiplied = document.getElementById('multiply')
 const subtracted = document.getElementById('subtract')
-const deleted = document.getElementById('del')
-operators = document.querySelectorAll('.operator')
+const hundred = document.getElementById('hundred')
+const displaytop = document.getElementById('displaytop')
+const displaybot = document.getElementById('displaybot')
 
+let inputnumber = []
+let operator = []
+let firstnumber = 0
+let secondnumber = 0
+let result= []
+let secondnumber1 = 0
+point.addEventListener('click',logInput)
+number7.addEventListener('click',logInput)
+number8.addEventListener('click',logInput)
+number9.addEventListener('click',logInput)
+number6.addEventListener('click',logInput)
+number5.addEventListener('click',logInput)                  // <-- bloco onde o se o botao é ativado faz a funcao mostrar
+number4.addEventListener('click',logInput)
+number3.addEventListener('click',logInput)
+number2.addEventListener('click',logInput)
+number1.addEventListener('click',logInput)
+number0.addEventListener('click',logInput)
+percented.addEventListener('click',logInput)
 
-
-
-let firstnumber = []
-let resultnow = []
-
-number7.addEventListener('click',showselection)
-number8.addEventListener('click',showselection)
-number9.addEventListener('click',showselection)
-number6.addEventListener('click',showselection)
-number5.addEventListener('click',showselection)                  // <-- bloco onde o se o botao é ativado faz a funcao mostrar
-number4.addEventListener('click',showselection)
-number3.addEventListener('click',showselection)
-number2.addEventListener('click',showselection)
-number1.addEventListener('click',showselection)
-number0.addEventListener('click',showselection)
-
-
-percented.addEventListener('click',showselection)
-point.addEventListener('click',showselection)
-plus.addEventListener('click',showselection,)
-divided.addEventListener('click',showselection)
-multiplied.addEventListener('click',showselection)
-subtracted.addEventListener('click',showselection)
+subtracted.addEventListener('click',logInput)
+plus.addEventListener('click',logInput)
+multiplied.addEventListener('click',logInput)
+divided.addEventListener('click',logInput)
+equals.addEventListener('click',logInput)
 cleared.addEventListener('click',clear)
 cleared2.addEventListener('click',clear)
+hundred.addEventListener('click', logInput)
 
+var operators = {
+    '+': function(a,b) {return parseFloat(a)+parseFloat(b)},
+    '-': function(a,b) {return a-b},
+    '*': function(a,b) {return a*b},
+    '/': function(a,b) {return a/b},
+    '%': function(a,b) {return a/100*b},
 
-
-var typed = []
-
-var separatedNumbers = []
-
-function showselection() {
-    value = this.value         
-    document.getElementById('displaytop').innerText += value   // < funcao mostrar
-    firstnumber.push(value)
-    typed.push(value)
-    
-    
-    
-    
-    console.log(value)
-    
-    if (value =='+'){
-        equals.onclick = sum  // se colocar o parantesis depois da funcao ele ativa na hora da deteccao da condicao e nao do clique.
-                            // provavelmente a solucao para alguns dos bugs.   
-                           
-    } else if (value == '-') {
-        equals.onclick = sub
-    } else if (value == '*') {
-        equals.onclick = multiply
-        
-    } else if (value == '/') {
-        equals.onclick = divide
-        
-    } else if (value == '%') {
-        equals.onclick = percent
-        
-    } 
-    
-    }
- 
-function separate(operator) {
-    let numerosString = firstnumber.toString()
-    let numerosStringConcatenados = numerosString.replace(/,/g ,'')
-    separatedNumbers=(numerosStringConcatenados.split(operator))       //separador com +
 
 }
 
-function sum() {       
- 
-        
-        separate('+')
-      
-     if (resultnow.length > 0) {
-       var a = resultnow[0]
-       
-       let i = 0
-            while (i < (separatedNumbers.length) ) {     // soma os elementos da array
-            
-            a += Number(separatedNumbers[i]);
-            i++;
-        }
-     } else {
-      
-       var a = Number(0)
-              
-       let i = 0
-            while (i < (separatedNumbers.length) ) {     // soma os elementos da array
-            
-            a += Number(separatedNumbers[i]);
-            i++;
-            }
-        
-        }
-        
+function clear(){ 
+    displaytop.innerHTML =''
+    displaybot.innerHTML =''
+    firstnumber =[]
+    secondnumber = []
+    operator = []
+    secondnumber1 =[]
     
-         print(a)
+}
+
+function logInput() {
+    if (operator.length == 0) {                                     // ****************************** NUMERO DE OPERADORES IGUAL A ZERO ***********************//
+            if (this.value =='+' || this.value == '-' || this.value =='*' || this.value =='/' || this.value =='%') {
+                operator.push(this.value)                                            // ADICIONA OPERADOR NA LISTA,
                 
-         firstnumber = []
-         typed = []
-        
-        
-                           
+                displaybot.innerHTML = displaytop.innerHTML                          // PASSA O PRIMEIRO NUMERO PARA DISPLAY DE BAIXO 
+                displaytop.innerHTML = this.value                                   // ADICIONA OS VALORES SEGUINTES PARA DISPLAY DE CIMA   
+                secondnumber = displaytop.innerHTML.slice(1,5)
+                console.log(secondnumber)
+                
+            }else {
+
+                if (this.value != '=') {
+                   
+                document.getElementById('displaytop').innerHTML += this.value           // ADICIONA O NUMERO NO DISPLAY DE CIMA
+                firstnumber = document.getElementById('displaytop').innerHTML           // O PRIMEIRO NUMERO, VAI SER O O QUE TIVER NO DISPLAY DE CIMA NO MOMENTO, ANTES DE CLICAR EM OPERADOR
+                console.log(secondnumber)}
+            
+                }
+ }else {                                                            //****************************** NUMERO DE OPERADORES MAIOR QUE ZERO ********************//
+
+        if (this.value == '+' || this.value == '-' || this.value == '*' || this.value == '/' || this.value =='%') {
+            operator.push(this.value)                                                               // ADICIONA O SEGUNDO OPERADOR NA LISTA 
+            console.log(operator)
+            displaybot.innerHTML = displaytop.innerHTML                                              // PASSA O SEGUNDO NUMERO + OPERADOR PRO DISPLAY DE BAIXO
+            displaytop.innerHTML = this.value 
+
+            secondnumber1 = secondnumber.slice(1)    
+            var op = operator[0]
+            result = operators[op](firstnumber,secondnumber1)
+            operator.shift()  
+            displaybot.innerHTML = +result.toFixed(2)                                 // RESULTADO VAI PRO DISPLAY DE BAIXO
+            firstnumber = +result.toFixed(2) 
+                                                              // RESULTADO VIRA O PRIMEIRO NUMERO
+            
+                       
+
+            
+
+        }else  if (this.value == '='){
+                      
+            
+            secondnumber1 = secondnumber.slice(1)
+            var op = operator[0]            
+            result = operators[op](firstnumber,secondnumber1)                                 
+            displaybot.innerHTML = +result.toFixed(2) 
+            firstnumber = +result.toFixed(2)                                     // RESULTADO VAI PRO DISPLAY DE BAIXO
+            secondnumber1 =[]
+            operator = []
+            console.log(firstnumber)
+            console.log(operator)
+            console.log(secondnumber)
+            displaytop.innerHTML = +result.toFixed(2)
+            secondnumber  =[]
+                                   
+        }
          
-    }
-
-function divide() {  
-       
-   if (resultnow.length>0) {
-    
-    separate('/')
-    
-        let i = 1
-        var a = (resultnow[0])
-        console.log(separatedNumbers)
-        
-        while (i < (separatedNumbers.length) ) {     // divide o primeiro elemento com os seguintes da array
-        
-        a /= Number(separatedNumbers[i]) ;
-        i++;
-        
-   
-    }
-  }else {
-
-    separate('/')
-    
-    let i = 1
-    var a = (separatedNumbers[0])
-    console.log(separatedNumbers)
-    
-    while (i < (separatedNumbers.length) ) {     // divide o primeiro elemento com os seguintes da array
-    
-    a /= Number(separatedNumbers[i]) ;
-    i++;
-    
-    }
-  }
-    print(a)
-    firstnumber = []
-
-    
-    
-    
-    
-    
-        
-    }
-function multiply() {       
-    
-    if(resultnow.length>0) {
-
-        separate('*')
-
-        var a = resultnow[0] 
-    
-        let i = 1
-                while (i < (separatedNumbers.length) ) {     // multiplica de o primeiro elemento com os seguintes da array
-                
-                a *= Number(separatedNumbers[i]);
-                i++;
-                
+        else {
+            document.getElementById('displaytop').innerHTML += this.value
+            secondnumber = document.getElementById('displaytop').innerHTML
             
             }
 
 
-
     }
-     else {
-    separate('*')
-    
-    
-    var a = Number(1) 
-    
-    let i = 0
-            while (i < (separatedNumbers.length) ) {     // multiplica de o primeiro elemento com os seguintes da array
-            
-            a *= Number(separatedNumbers[i]);
-            i++;
-            
-        
-        }
-    }
-        print(a)
-        firstnumber = []
-        typed = []
 }
-function sub() {       
-    
-            
-    separate('-')
-    if (resultnow.length > 0)  {
-    
-     a = parseInt(resultnow[0])
-    
-        let i = 1
-         while (i < (separatedNumbers.length) ) {     // diminui o primeiro elemento com os outros
-         a -= Number(separatedNumbers[i]);
-         i++;     
-                  
-       
-         
-         }
-     } else {
-
-        a = separatedNumbers[0]
-    
-        let i = 1
-         while (i < (separatedNumbers.length) ) {     // diminui o primeiro elemento com os outros
-         a -= Number(separatedNumbers[i]);
-         i++;     
-                  
-       
-         
-         }
-
-
-     }
-     print(a)
-     firstnumber = []
-        
-    
- 
- }
- function clear() {       
-      
-        
-       document.getElementById('displaybot').innerHTML= ''
-       document.getElementById('displaytop').innerHTML= ''
-       firstnumber = []
-       resultnow = []
-     
-
-      
- }
- function percent() {       
-    
-    
-    separate('%')   
-   
-   
-    let a = Number(separatedNumbers[0])
-    
-        let i = 1
-         while (i < (separatedNumbers.length) ) {     // diminui o primeiro elemento com os outros
-         a = a*0.01*Number(separatedNumbers[i]);
-         i++;
-         
-     
-     }
-     document.getElementById('displaybot').innerHTML = a
-     
-
-      
- }
-function print(a){
- 
-    document.getElementById('displaybot').innerHTML = a
-    resultnow.unshift(a)
-    console.log(resultnow)
-    document.getElementById('displaytop').innerHTML = ''
-   
-   
-}
-
